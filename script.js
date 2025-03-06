d49d877a5156c07d062816770c0f1375885591bf
Overt$!#& <135649678+InvertOvertJM@users.noreply.github.com>
2025-03-06 10:59:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//dad joke api
 const jokeText = document.getElementById('dadJoke')
 const userText = document.getElementById('info')
 const dogImage = document.getElementById('dogImg')
@@ -14,19 +15,21 @@ fetch('https://icanhazdadjoke.com/', {
     })
     .catch(error => console.error('Error:', error));
 
+//random user api
 fetch('https://randomuser.me/api/')
     .then(response => response.json()) 
     .then(data => {
         console.log('Name:', data.results[0].name.first + ' ' + data.results[0].name.last);
         console.log('Email:', data.results[0].email);
         console.log('Location:', data.results[0].location.city);
-        userText.textContent = ('Name:', data.results[0].name.first + ' ' + data.results[0].name.last);
+        userText.textContent = ('Name:', data.results[0].name.first + ' ' + data.results[0].name.last); //just the name of the user
     })
     .catch(error => console.error('Error:', error));
 
+//dog ceo api
 fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
     .then(data => {
-        document.getElementById('dogImg').src = data.message;
+        document.getElementById('dogImg').src = data.message; //dog image replacer
     })
     .catch(error => console.error('Error:', error));
