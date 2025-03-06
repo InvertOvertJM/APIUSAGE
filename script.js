ac136da0dece03aec4008acc3b813bba10b6a1aa
Overt$!#& <135649678+InvertOvertJM@users.noreply.github.com>
2025-03-06 08:21:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const jokeText = document.getElementById('dadJoke')
+const userText = document.getElementById('info')
+const dogImage = document.getElementById('dogImg')
+
+fetch('https://icanhazdadjoke.com/', {
+    headers: {
+        'Accept': 'application/json'
+    }
+})
+    .then(response => response.json())  
+    .then(data => {
+        console.log(data.joke);
+        jokeText.textContent = data.joke;
+    })
+    .catch(error => console.error('Error:', error));
+
+fetch('https://randomuser.me/api/')
+    .then(response => response.json()) 
+    .then(data => {
+        console.log('Name:', data.results[0].name.first + ' ' + data.results[0].name.last);
+        console.log('Email:', data.results[0].email);
+        console.log('Location:', data.results[0].location.city);
+        userText.textContent = ('Name:', data.results[0].name.first + ' ' + data.results[0].name.last);
+    })
+    .catch(error => console.error('Error:', error));
+
+fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('dogImg').src = data.message;
+    })
+    .catch(error => console.error('Error:', error));
